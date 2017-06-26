10522658869fe373b06d049e4deb97d60f04b135
Sam Chung <chungsam@outlook.com>
2017-06-26 11:19:50 -0700

    Add Rationale pages

--- js/intro/app.js
+++ js/intro/app.js
@@ -13,8 +13,25 @@ $(document).ready(function() {
     $('#pagepiling').pagepiling({
         // Pass in an options object when initializing
         menu: "#menu",
-        anchors: ['homepage', 'team-intro', 'rotate-page', 'text-page'],
-        sectionsColor: ['whitesmoke', 'whitesmoke', 'whitesmoke',  'whitesmoke'],
+        anchors: [
+            'homepage', 
+            'team-intro', 
+            'rationale-intro', 
+            , // empty anchors because these aren't visible in main nav
+            , 
+            , 
+            , 
+            'link-to-proto'],
+        sectionsColor: [
+            'whitesmoke', // home
+            'whitesmoke', // team-intro
+            'whitesmoke', // rationale-intro
+            'whitesmoke', // rationale-main-1
+            'whitesmoke', // rationale-main-2
+            'whitesmoke', // rationale-main-3
+            'whitesmoke', // rationale-main-4
+            'whitesmoke'  // link-to-proto
+            ], 
         verticalCentered: false
     });
 })
